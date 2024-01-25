c5fa332c85e77779f2a264773a601aefb5139802
rwaldron <outsideinprofits@gmail.com>
2024-01-25 14:44:34 +1100

    feat: :sparkles: Added Examples page

---
+++ app/examples/page.tsx
@@ -0,0 +1,9 @@
+export default async function Index() {
+  return (
+    <div className="flex flex-col items-center flex-1 w-full gap-20">
+      <div className="flex flex-col flex-1 max-w-4xl gap-20 px-3">
+        <h1 className="text-xl text-cyan-600">Examples page</h1>
+      </div>
+    </div>
+  );
+}
