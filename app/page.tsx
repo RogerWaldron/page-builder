75115a7f2daadcbcf0a6be012836673695774848
Roger Waldron <87526672+RogerWaldron@users.noreply.github.com>
2024-01-26 04:42:31 +0400

    Landing page skeleton (#4)
    * feat: :sparkles: Added PageHeader component
    
    * feat: :sparkles: Added PageActions component
    
    * style: :lipstick: Removed max width from component
    
    * build: :heavy_plus_sign: Added React-Wrap-Balancer library
    
    * style: :lipstick: Cleaned up styles
    
    * style: :lipstick: Removed max width
    
    * feat: :lipstick:
    
    ---------
    
    Co-authored-by: rwaldron <outsideinprofits@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+import PageActions from "@/components/PageActions";
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/PageHeader";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
 export default async function Index() {
   return (
-    <div className="flex flex-col items-center flex-1 w-full gap-20">
-      <div className="flex flex-col flex-1 max-w-4xl gap-20 px-3">
-        <h1 className="text-xl text-cyan-600">landing page</h1>
-      </div>
+    <div className="container relative">
+      <PageHeader>
+        <PageHeaderHeading>Build your web page</PageHeaderHeading>
+        <PageHeaderDescription>
+          Beautifully designed components that you can drag and drop.
+        </PageHeaderDescription>
+        <PageActions>
+          <Link href="/login" className={cn(buttonVariants())}>
+            Sign Up. It's Free!
+          </Link>
+          <Link
+            href="/docs"
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
+            Learn More
+          </Link>
+        </PageActions>
+      </PageHeader>
     </div>
   );
 }
