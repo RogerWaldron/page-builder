4924df4c33b4fe615ad433ddce6766f8b7d15173
rwaldron <outsideinprofits@gmail.com>
2024-01-26 11:37:10 +1100

    style: :lipstick: Removed max width

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,7 +1,7 @@
 export default async function Index() {
   return (
     <div className="flex flex-col items-center flex-1 w-full gap-20">
-      <div className="flex flex-col flex-1 max-w-4xl gap-20 px-3">
+      <div className="flex flex-col flex-1 gap-20 px-3">
         <h1 className="text-xl text-cyan-600">Docs page</h1>
       </div>
     </div>
