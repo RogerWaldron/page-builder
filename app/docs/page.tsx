b40423f7285c5b76c62efa6d54dd808f70397bf9
rwaldron <outsideinprofits@gmail.com>
2024-01-26 13:15:45 +1100

    refactor: :fire: Removed Docs page template

--- app/docs/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Index() {
-  return (
-    <div className="flex flex-col items-center flex-1 w-full gap-20">
-      <div className="flex flex-col flex-1 gap-20 px-3">
-        <h1 className="text-xl text-cyan-600">Docs page</h1>
-      </div>
-    </div>
-  );
-}
