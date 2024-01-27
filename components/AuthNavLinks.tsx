f0efc99bef64a8e735c713316a44836796f983a1
rwaldron <outsideinprofits@gmail.com>
2024-01-27 20:25:01 +1100

    feat: :sparkles: Added Link for Photos route

--- components/AuthNavLinks.tsx
+++ components/AuthNavLinks.tsx
@@ -1,22 +1,24 @@
-import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
-import { cookies } from "next/headers";
+import { cn } from "@/lib/utils";
+import { getUserLoggedIn } from "@/lib/supabaseUtils";
+import { getBrowserURL } from "@/lib/client";
 
 export default async function AuthNavLinks() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const user = await getUserLoggedIn();
 
   return user ? (
-    <div className="flex items-center gap-4">
+    <div className="flex items-center gap-4 mx-4">
       <Link
         href="/dashboard"
-        className="mr-4 transition-colors text-foreground/60 hover:text-foreground/80"
+        className="transition-colors hover:text-foreground/80"
       >
         Dashboard
+      </Link>
+      <Link
+        href="/photos"
+        className="transition-colors hover:text-foreground/80"
+      >
+        Photos
       </Link>
     </div>
   ) : (
